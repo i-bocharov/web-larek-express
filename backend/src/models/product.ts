b6cea3010618edd7a363f88db58e0fd9a56763b8
Igor Bocharov <igor.bocharov56@yandex.ru>
2025-09-02 22:55:59 +0500

    feat(product):  создал схему и модель product

---
+++ backend/src/models/product.ts
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+
+interface IProduct {
+  title: string;
+  image: {
+    fileName: string;
+    originalName: string;
+  };
+  category: string;
+  description?: string;
+  price?: number | null;
+}
+
+const productSchema = new mongoose.Schema<IProduct>({
+  title: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+    unique: true,
+  },
+  image: {
+    type: {
+      fileName: {
+        type: String,
+        required: true,
+      },
+      originalName: {
+        type: String,
+        required: true,
+      },
+    },
+    required: true,
+    _id: false,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  price: {
+    type: Number,
+    default: null,
+  },
+});
+
+export default mongoose.model<IProduct>('product', productSchema);
