a8e4b9321bd2e57d04bcf7156404af83b29c4369
Igor Bocharov <igor.bocharov56@yandex.ru>
2025-09-02 22:12:35 +0500

    feat: подключился к MongoDB

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+// подключаемся к серверу MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/weblarek');
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
