630f954ca9d4fbd59f019c585cb57026331d5f79
Igor Bocharov <igor.bocharov56@yandex.ru>
2025-09-06 00:50:03 +0500

    feat(order): добавил бизнес-логику в контроллер order

--- backend/src/controllers/order.ts
+++ backend/src/controllers/order.ts
@@ -2,6 +2,8 @@ import { Request, Response, NextFunction } from 'express';
 import mongoose from 'mongoose';
 import { faker } from '@faker-js/faker';
 import BadRequestError from '../errors/bad-request-error';
+import NotFoundError from '../errors/not-found-error';
+import product from '../models/product';
 
 /**
  * Создание заказа
@@ -9,9 +11,43 @@ import BadRequestError from '../errors/bad-request-error';
  * @param res - объект ответа
  * @param next - функция для передачи управления следующему middleware
  */
-const createOrder = (req: Request, res: Response, next: NextFunction) => {
+const createOrder = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { total } = req.body;
+    const { total, items } = req.body;
+
+    // Проверяем, что _id существуют в базе
+    const products = await product.find({ _id: { $in: items } }).lean();
+
+    // Если количество найденных товаров не совпадает с количеством ID в запросе,
+    // значит, какой-то ID недействителен.
+    if (products.length !== items.length) {
+      return next(
+        new NotFoundError(
+          'Один или несколько товаров не найдены в базе данных',
+        ),
+      );
+    }
+
+    // Проверяем, что товары продаются, и считаем сумму
+    const actualTotal = products.reduce((sum, productItem) => {
+      if (productItem.price === null) {
+        // Выбрасываем ошибку, если товар недоступен
+        throw new BadRequestError(
+          `Товар "${productItem.title}" недоступен для покупки (цена не указана)`,
+        );
+      }
+      return sum + (productItem.price ?? 0);
+    }, 0);
+
+    // Проверяем, что стоимость переданных товаров в сумме равна total
+    if (actualTotal !== total) {
+      return next(
+        new BadRequestError(
+          `Сумма заказа не совпадает: ожидается ${actualTotal}, получено ${total}`,
+        ),
+      );
+    }
+
     // Генерация случайного UUID для заказа.
     const orderId = faker.string.uuid();
 
