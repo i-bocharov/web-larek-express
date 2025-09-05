8da420952237bfc02da2e0135c827f69101579a9
Igor Bocharov <igor.bocharov56@yandex.ru>
2025-09-06 00:08:15 +0500

    feat(order): добавил валидацию данных через celebrate и joi

--- backend/src/routes/order.ts
+++ backend/src/routes/order.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import createOrder from '../controllers/order';
+import createOrderValidator from '../middlewares/validation';
 
 const orderRouter = Router();
 
-orderRouter.post('/order', createOrder);
+orderRouter.post('/order', createOrderValidator, createOrder);
 
 export default orderRouter;
