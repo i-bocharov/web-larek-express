440cbb9bfd11838b89e5e38f6511f9ec7c3f8fdc
Igor Bocharov <igor.bocharov56@yandex.ru>
2025-09-06 21:55:35 +0500

    refactor: устранил замечания после ревью

--- backend/src/models/product.ts
+++ backend/src/models/product.ts
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose';
 
+interface IImage {
+  fileName: string;
+  originalName: string;
+}
+
 interface IProduct {
   title: string;
-  image: {
-    fileName: string;
-    originalName: string;
-  };
+  image: IImage;
   category: string;
   description?: string;
   price?: number | null;
